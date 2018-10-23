23554d45bb615d091f5ace1324e19005da76ad7e
Nick <Ecton.Nick@gmail.com>
2018-10-23 11:28:30 -0700

    Complete events API and some of eventactions

---
+++ frontend/actions/event_actions.js
@@ -0,0 +1,51 @@
+export const RECEIVE_EVENTFUL = 'RECEIVE_EVENTFUL'
+export const RECEIVE_GOOGLE = 'RECEIVE_GOOGLE'
+export const RECEIVE_GOOGLE_IMAGE = 'RECEIVE_GOOGLE_IMAGE'
+export const RECEIVE_EVENTFUL_IMAGE = 'RECEIVE_EVENTFUL_IMAGE'
+import * as ApiUtil from '../util/api/events.js'
+
+
+const receiveMultipleEventful = (events) => ({
+    type: RECEIVE_EVENTFUL,
+    events
+})
+
+const receiveTheEventfulImage = (image) => ({
+    type: RECEIVE_EVENTFUL_IMAGE,
+    image
+})
+
+const receiveMultipleGoogle = (events) => ({
+    type: RECEIVE_GOOGLE,
+    events
+})
+
+const receiveTheGoogleImage = (image) => ({
+    type: RECEIVE_GOOGLE_IMAGE,
+    image
+)}
+
+export const recieveEventful = (category, location) => dispatch => {
+   return ApiUtil.receiveEventful(category, location).then((res) => {
+        dispatch(receiveMultipleEventful(res))
+    })
+}
+
+export const receiveGooglePlaces = (type, location) => dispatch => {
+    return ApiUtil.receiveGooglePlaces(type, location).then((res) => {
+        dispatch(receiveMultipleGoogle(res))
+    })
+}
+
+export const receiveEventfulImage = (id) => dispatch => {
+    return ApiUtil.receiveEventfulImage(id).then((res) => {
+        dispatch(receiveTheEventfulImage(res))
+    })
+}
+
+export const receiveGoogleImage = (id) => dispatch => {
+    return ApiUtil.receiveGoogleImage(id).then((res) => {
+        dispatch(receiveTheGoogleImage(res))
+    })
+}
+
--- frontend/util/api/events.js
+++ frontend/util/api/events.js
@@ -1,13 +1,13 @@
-const receiveEventful = (category, location) => {
+export const receiveEventful = (category, location) => {
     return (
         $.ajax({
         method: "GET",
-        url: `http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&q=${category}&location=${location}&date=Today`
+        url: `http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&q=${category}&location=${location[0]},${location[1]}&date=Today`
         })
     )
 }
 
-const receiveEventfulImage = (id) => {
+export const receiveEventfulImage = (id) => {
     return (
         $.ajax({
         method: "GET",
@@ -16,16 +16,16 @@ const receiveEventfulImage = (id) => {
     )
 }
 
-const receiveGooglePlaces = (type, location) => {
+export const receiveGooglePlaces = (type, location) => {
     return (
         $.ajax({
             method: "GET",
-            url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4&location=${location}&type=${type}`
+            url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4&location=${location[0]},${location[1]}&type=${type}`
         })
     )
 }
 
-const receiveGoogleImage = (ref) => {
+export const receiveGoogleImage = (ref) => {
     return (
         $.ajax({
             method: "GET",
@@ -33,3 +33,31 @@ const receiveGoogleImage = (ref) => {
         })
     )
 }
+
+export const receiveHiking = (location) => (
+    $.ajax({
+      method: 'GET',
+      url: `https://www.hikingproject.com/data/get-trails?lat=${location[0]}&long=${location[1]}&key=200376730-1322e2d6ca80c8e55d1a282398106088&maxDistance=200`
+    })
+   )
+   
+   export const receiveTour = (location) => (
+    $.ajax({
+      method: 'GET',
+      url: `https://api.izi.travel/mtg/objects/search?form=short&type=tour&includes=location&except=city,country,publisher&languages=en&lat_lon=${location[0]},${location[1]}&radius=10000&api_key=ff1c5c19-1fd2-4383-a810-3cebbf5819e6`
+    })
+   )
+   
+   export const receiveTourImage = (img_get_req) => (
+    $.ajax({
+      method: 'GET',
+      url: img_get_req
+    })
+   )
+   
+   export const receiveEvent = (location) => (
+    $.ajax({
+      method: 'GET',
+      url: `https://api.predicthq.com/v1/events/?limit=5&within=15km@${location[0]},${location[1]}`
+    })
+   )
