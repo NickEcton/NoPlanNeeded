8d8225fb3482b89f47ef07ac6d4a3ef6007df460
Devin Flood <devin.p.flood@gmail.com>
2018-10-22 21:59:35 -0700

    Add home page and nav bar components

--- frontend/src/components/home_page/home_page.jsx
+++ frontend/src/components/home_page/home_page.jsx
@@ -7,7 +7,6 @@ import npnlogo from '../../images/npn-logo-1.1.png'
 export default () => {
     return (
         <div className='home-page-div'>
-            <h1>Home Page</h1>
             <img 
                 className='event-button'
                 src={npnlogo}
--- frontend/src/components/nav_bar/nav_bar.jsx
+++ frontend/src/components/nav_bar/nav_bar.jsx
@@ -3,12 +3,30 @@ import { Link } from 'react-router-dom';
 
 import '../../stylesheets/nav_bar.css'
 import npnlogo from '../../images/npn-logo-3.1.png'
+import menuicon from '../../images/npn-menu-icon.png'
 
 export default ({ currentUser }) => {
-    return (
+    if  (true) {
+        return(
+            <div className='nav-bar-div'>
+                <img className='nav-logo' src={npnlogo} alt='npn logo'/>
+                <div className='drop-down'>
+                    <img className='menu-icon' src={menuicon} alt='menu icon'/>
+                    <div className='drop-down-content'>
+                        <h1>Account</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return(
         <div className='nav-bar-div'>
-            <img className='nav-logo' src={npnlogo} alt='npn logo'/>
-            <h1>Nav Bar</h1>
+            <img className='nav-logo' src={npnlogo} alt='npn logo' />
+            <div className='login-signup'>
+                <h1>Login</h1> 
+                <h1>Signup</h1>
+            </div>
         </div>
-    );
+        );
+    }
 }
