17b455e5ab8f4db413b815dd103773c640f18e7c
Stephanie Hsu <hsusteph5@gmail.com>
2018-10-22 20:00:37 -0700

    Add signup form container and login form container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Root from './components/Root.jsx';
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
          <Provider store={this.props.store}>
--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route, Redirect, Switch, HashRouter } from 'react-router-dom';
+import LoginFormContainer from './session_form/login_form_container.js';
+import SignupFormContainer from './session_form/signup_form_container.js';
+//in this file, we will putting in the modals and the routes
 
-const Root = () => (
+const Root = () => {
+    return(
     <div>
         <h1> Hello world!!!!!!!!</h1>
-        
+        <Switch>
+            <Route exact path = "/login" component={LoginFormContainer} />
+            <Route exact path = "/signup" component={SignupFormContainer} />
+        </Switch>
     </div>
-)
+
+    );
+}
 
 export default Root;
