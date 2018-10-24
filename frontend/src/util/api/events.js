a79598bdae09d493f31ed4b3b1493bebbd4e92d5
Stephanie Hsu <hsusteph5@gmail.com>
2018-10-24 10:24:16 -0700

    making axios request template

--- frontend/src/util/api/events.js
+++ frontend/src/util/api/events.js
@@ -1,63 +1,97 @@
-export const receiveEventful = (category, location) => {
-    return (
-        $.ajax({
-        method: "GET",
-        url: `http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&q=${category}&location=${location[0]},${location[1]}&date=Today`
-        })
-    )
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+export const receiveImage = (id) => {
+  return (
+    axios({
+      method: 'GET',
+      url: `/api/events/test/${id}`,
+      // headers: ???
+    })).then(res => console.log(res))
 }
 
-export const receiveEventfulImage = (id) => {
-    return (
-        $.ajax({
-        method: "GET",
-        url: `http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&id=${id}`
-        })
-    )
-}
+// https://github.com/axios/axios/issues/853
 
-export const receiveGooglePlaces = (type, location) => {
-    return (
-        $.ajax({
-            method: "GET",
-            url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4&location=${location[0]},${location[1]}&type=${type}`
-        })
-    )
-}
 
-export const receiveGoogleImage = (ref) => {
-    return (
-        $.ajax({
-            method: "GET",
-            url: `https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4&photoreference=${ref}&maxheight=300`
-        })
-    )
-}
+// export const receiveEventful = (category, location) => {
+//     return (
+//         $.ajax({
+//         method: "GET",
+//         url: `http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&q=${category}&location=${location[0]},${location[1]}&date=Today`
+//         })
+//     )
+// }
 
-export const receiveHiking = (location) => (
-    $.ajax({
-      method: 'GET',
-      url: `https://www.hikingproject.com/data/get-trails?lat=${location[0]}&long=${location[1]}&key=200376730-1322e2d6ca80c8e55d1a282398106088&maxDistance=200`
-    })
-   )
-   
-   export const receiveTour = (location) => (
-    $.ajax({
-      method: 'GET',
-      url: `https://api.izi.travel/mtg/objects/search?form=short&type=tour&includes=location&except=city,country,publisher&languages=en&lat_lon=${location[0]},${location[1]}&radius=10000&api_key=ff1c5c19-1fd2-4383-a810-3cebbf5819e6`
-    })
-   )
-   
-   export const receiveTourImage = (img_get_req) => (
-    $.ajax({
-      method: 'GET',
-      url: img_get_req
-    })
-   )
-   
-   export const receiveEvent = (location) => (
-    $.ajax({
-      method: 'GET',
-      url: `https://api.predicthq.com/v1/events/?limit=5&within=15km@${location[0]},${location[1]}`
-    })
-   )
+// export const receiveEventfulImage = (id) => {
+//     return (
+//         $.ajax({
+//         method: "GET",
+//         url: `http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&id=${id}`
+//         })
+//     )
+// }
+
+// export const receiveGooglePlaces = (type, location) => {
+//     return (
+//         $.ajax({
+//             method: "GET",
+//             url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4&location=${location[0]},${location[1]}&type=${type}`
+//         })
+//     )
+// }
+
+// export const receiveGoogleImage = (ref) => {
+//     return (
+//         $.ajax({
+//             method: "GET",
+//             url: `https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4&photoreference=${ref}&maxheight=300`
+//         })
+//     )
+// }
+
+// export const receiveHiking = (location) => (
+//     $.ajax({
+//       method: 'GET',
+//       url: `https://www.hikingproject.com/data/get-trails?lat=${location[0]}&long=${location[1]}&key=200376730-1322e2d6ca80c8e55d1a282398106088&maxDistance=200`
+//     })
+//    )
+
+//    export const receiveTour = (location) => (
+//     $.ajax({
+//       method: 'GET',
+//       url: `https://api.izi.travel/mtg/objects/search?form=short&type=tour&includes=location&except=city,country,publisher&languages=en&lat_lon=${location[0]},${location[1]}&radius=10000&api_key=ff1c5c19-1fd2-4383-a810-3cebbf5819e6`
+//     })
+//    )
+
+//    export const receiveTourImage = (img_get_req) => (
+//     $.ajax({
+//       method: 'GET',
+//       url: img_get_req
+//     })
+//    )
+
+//    export const receiveEvent = (location) => (
+//     $.ajax({
+//       method: 'GET',
+//       url: `https://api.predicthq.com/v1/events/?limit=5&within=15km@${location[0]},${location[1]}`
+//     })
+//    )
+
+
+
+export const RECEIVE_IMAGE = 'RECEIVE_IMAGE';
+
+export const receiveCurrentImage = id => {
+    return {
+      type: RECEIVE_IMAGE,
+      payload: id
+    };
+  };
+//testing axios
+// export const receiveImage = (id) => {
+//     axios
+//       .get(`http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&id=${id}`, id)
+//     //   .then(res => {
+//     //     dispatch(receiveCurrentImage(res.data));
+//     //   })
+//   };
