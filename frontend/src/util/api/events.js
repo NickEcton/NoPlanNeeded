9f5f26bb34dfdff9cb5c721df642feaa4debe2af
Clayton Christian <claytonchristian11@gmail.com>
2018-10-25 10:07:07 -0700

    Reformat ajax reqs to axios reqs

--- frontend/src/util/api/events.js
+++ frontend/src/util/api/events.js
@@ -10,6 +10,51 @@ export const receiveImage = (id) => {
     })).then(res => console.log(res))
 }
 
+export const receiveHiking = (location) => {
+  let loc1 = location[0]
+  let loc2 = location[1]
+  return(
+    axios({
+      method: 'GET',
+      url: `/hiking/${location}`
+    })
+  )
+}
+
+export const receiveTour = (location) => {
+   let loc1 = location[0]
+   let loc2 = location[1]
+   return (
+      axios({
+        method: 'GET',
+        url: `/tour/${location}`
+      })
+   )
+}
+
+export const receiveEvent = (location) => {
+   let loc1 = location[0]
+   let loc2 = location[1]
+   return (
+      axios({
+        method: 'GET',
+        url: `/predict/${location}`,
+        headers: {
+          Authorization: 'Bearer wEdKqev9Hvs8OxVAzaIhZnqpTfhDjW'
+        }
+      })
+   )
+}
+
+export const receiveTourImage = (img_get_req) => (
+  axios({
+    method: 'GET',
+    url: `/tourImage/${img_get_req}`
+  })
+)
+
+
+
 // https://github.com/axios/axios/issues/853
 
 
@@ -49,33 +94,12 @@ export const receiveImage = (id) => {
 //     )
 // }
 
-// export const receiveHiking = (location) => (
-//     $.ajax({
-//       method: 'GET',
-//       url: `https://www.hikingproject.com/data/get-trails?lat=${location[0]}&long=${location[1]}&key=200376730-1322e2d6ca80c8e55d1a282398106088&maxDistance=200`
-//     })
-//    )
 
-//    export const receiveTour = (location) => (
-//     $.ajax({
-//       method: 'GET',
-//       url: `https://api.izi.travel/mtg/objects/search?form=short&type=tour&includes=location&except=city,country,publisher&languages=en&lat_lon=${location[0]},${location[1]}&radius=10000&api_key=ff1c5c19-1fd2-4383-a810-3cebbf5819e6`
-//     })
-//    )
 
-//    export const receiveTourImage = (img_get_req) => (
-//     $.ajax({
-//       method: 'GET',
-//       url: img_get_req
-//     })
-//    )
 
-//    export const receiveEvent = (location) => (
-//     $.ajax({
-//       method: 'GET',
-//       url: `https://api.predicthq.com/v1/events/?limit=5&within=15km@${location[0]},${location[1]}`
-//     })
-//    )
+
+
+
 
 
 
--- routes/api/events.js
+++ routes/api/events.js
@@ -7,16 +7,65 @@ const axios = require('axios');
 const Event = require('../../models/Event');
 const validateEventInput = require('../../validations/events');
 
-//work on this! 
+//work on this!
 router.get('/test/:id', (req, res) => {
   axios({
     method: 'GET',
     url: `http://eventful.com/json/events/?app_key=VQSPqhzDdNq9cW4t&id=${req.params.id}`,
   }).then(response => {
-    // debugger;
     return res.send(response);
   })
 });
+
+router.get('/hiking/:location', (req, res) => {
+  let loc1 = req.params.location[0]
+  let loc2 = req.params.location[1]
+  axios({
+    method: 'GET',
+    url: `https://www.hikingproject.com/data/get-trails?lat=${loc1}&long=${loc2}&key=200376730-1322e2d6ca80c8e55d1a282398106088&maxDistance=200`
+  }).then(response => {
+    return res.send(response.data);
+  })
+});
+
+router.get('/tour/:location', (req, res) => {
+  axios({
+    method: 'GET',
+    url: `https://api.izi.travel/mtg/objects/search?form=short&type=tour&includes=location&except=city,country,publisher&languages=en&lat_lon=${req.params.location}&radius=10000&api_key=ff1c5c19-1fd2-4383-a810-3cebbf5819e6`
+  }).then(response => {
+    return res.send(response.data);
+  })
+});
+
+router.get('/predict/:location', (req, res) => {
+  axios({
+    method: 'GET',
+    url: `https://api.predicthq.com/v1/events/?limit=5&within=15km@${req.params.location}`,
+    headers: {
+      Authorization: 'Bearer wEdKqev9Hvs8OxVAzaIhZnqpTfhDjW'
+    }
+  }).then(response => {
+    return res.send(response.data);
+  })
+});
+
+router.get('/tourImage/:getreq', (req, res) => {
+  axios({
+    method: 'GET',
+    url: `https://api.predicthq.com/v1/events/?limit=5&within=15km@${req.params.location}`,
+    headers: {
+      Authorization: 'Bearer wEdKqev9Hvs8OxVAzaIhZnqpTfhDjW'
+    }
+  }).then(response => {
+    return res.send(response.data);
+  })
+});
+
+
+
+
+
+
 
 
 module.exports = router;
