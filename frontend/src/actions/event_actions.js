e19a4d2e0caaabee201cb8e0d206778a6e696dc5
Nick <Ecton.Nick@gmail.com>
2018-10-26 17:20:07 -0700

    fixing google images

--- frontend/src/actions/event_actions.js
+++ frontend/src/actions/event_actions.js
@@ -4,6 +4,9 @@ import eventfulNormalizer from '../components/normalizers/eventfulNormalizer.js'
 import tourNormalizer from '../components/normalizers/tourNormalizer.js';
 import hikingNormalizer from '../components/normalizers/hikingNormalizer.js';
 import predictNormalizer from '../components/normalizers/predictNormalizer.js';
+const RapidAPI = require('rapidapi-connect');
+const rapid = new RapidAPI;
+
 
 
 export const RECEIVE_ONE_EVENT = 'RECEIVE_ONE_EVENT'
@@ -29,38 +32,54 @@ export const pickRandomEvent = (pojo) => {
     function sample(array) {
         return array[Math.floor ( Math.random() * array.length )]
         }
+        
+        
 
-        let choice = sample(options.sample(pojo.categories))
+        let choice = sample(options[sample(pojo.categories)])
 
         if (["concerts", "sports", "comedy"].includes(choice)) {
-            receiveEventful(pojo.location, choice)
+            return receiveEventful(pojo.location, choice)
         } else if (choice === "tour") {
-            receiveTour(pojo.location)
+            return receiveTour(pojo.location)
         } else if (choice === "hiking") {
-            receiveHiking(pojo.location)
+            return receiveHiking(pojo.location)
         } else if (choice === "random") {
-            receiveEvent(pojo.location)
+            return receiveEvent(pojo.location)
         } else {
-            receiveGooglePlaces(pojo.location, choice)
+            return receiveGooglePlaces(pojo.location, choice)
         }
-
+        
 }
 
 export const receiveGooglePlaces = (location, category) => dispatch => {
     
     return ApiUtil.receiveGooglePlaces(category, location).then((res) => { 
         let pojo = googleNormalizer(res)
-        
-        if (pojo.photoref) {
-        
-            ApiUtil.receiveGoogleImage(pojo.photoref).then((res) => {
             
-                pojo["photo"] = res
+        if (pojo.photoref) {
+            debugger
+            //     rapid.call('GooglePlaces', 'getImageURL', { 
+            //         'apiKey': 'AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4',
+            //         'max_width': '300',
+            //         'image_id': 'CmRaAAAAgGJ8-ERneeVWPO8YD8j7_oq55aMwy9CkJYyb9T9NaW076gjDxTonV91hZ6CNK_R_UTbZFWyz5umZiqHI0jrKor24gyffPv8JrCo7kdoPh7LXvgbcsJwUBwbRqXQIUJkSEhADcVfJhGI6sC17EGK6uRncGhSiyeuYSHoBpBjfgxaIgP7fx6h_Kw',
+            //         'max_height': '300'
+                
+            //     }).on('success', (payload)=>{
+            //         console.log('s')
+            //         console.log(payload)
+            //    }).on('error', (payload)=>{
+            //        console.log('e')
+            //         console.log(payload)
+            //    });
+            
+            // ApiUtil.receiveGoogleImage(pojo.photoref).then((res) => {
+                pojo["photo"] =`https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyDYpYUFcyLs-NsGwq7CYkPLFSnGcZ5unp4&photoreference=${pojo.photoref}&maxheight=300`
+                
                 dispatch(receiveOneEvent(pojo))
-            })
-        } else {
-            dispatch(receiveOneEvent(pojo))
-        }
+        //     })
+        // } else {
+        //     dispatch(receiveOneEvent(pojo))
+         }
 
     })
 }
@@ -73,7 +92,7 @@ export const receiveEventful = (location, category) => dispatch => {
         let pojo = eventfulNormalizer(res)       
         
         ApiUtil.receiveEventfulImage(pojo.id).then((res) => {
-
+            
             pojo["location"] = [res.data.latitude, res.data.longitude]
             
             if (res.data.images) {
