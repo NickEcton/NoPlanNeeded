64fc924be2828e3e8467028edc216254465f6f02
Stephanie Hsu <hsusteph5@gmail.com>
2018-11-13 11:41:17 -0800

    Delete extraneous code

--- frontend/src/reducers/entities_reducer/preferences_reducer.js
+++ frontend/src/reducers/entities_reducer/preferences_reducer.js
@@ -1,6 +1,5 @@
 import { RECEIVE_PREFERENCE } from '../../actions/preferences_actions.js';
 import merge  from 'lodash/merge'; 
-// import { RECEIVE_CURRENT_USER } from '../../util/session_api_util.js';
 
 //normalize the array of preferences that comes in through an array 
 //preferences are stored by id 
@@ -21,27 +20,6 @@ const normalizeBackend = (preference) => {
 }
 
 
-// const normalizePreference = (preference) => {
-<<<<<<< HEAD
-//     debugger;
-=======
->>>>>>> origin
-//     return { 
-//         [preference.id]: {
-//         id: preference.id, 
-//         adult: preference.adult,
-//         concerts: preference.concerts,
-//         familyFriendly: preference.familyFriendly, 
-//         food: preference.food,
-//         historic: preference.historic, 
-//         outdoors: preference.outdoors, 
-//         sports: preference.sports,
-//         random: preference.random,
-//         userId: preference.userId
-//     }}
-// }
-
-
 const preferencesReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type) {
@@ -49,12 +27,6 @@ const preferencesReducer = (state = {}, action) => {
             let preference = normalizeBackend(action.preference);
             let newState = merge({}, state, preference);
             return newState;
-        //in the case when the user logins, the preferences should update
-        // case RECEIVE_CURRENT_USER: 
-        //     debugger;
-            // preference = normalizePreference(action.payload.preference);
-            // newState = merge({}, state, preference);
-            return state;
         default:
             return state;
     }
